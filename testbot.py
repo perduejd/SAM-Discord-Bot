8c547302b8dc769cfe18e651c9bd0edee76dd388
Jacob Perdue <113634959+perduejd@users.noreply.github.com>
2023-10-23 20:47:37 -0400

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -21,3 +21,44 @@ async def hello(ctx):
 
 # Run the bot with your token
 bot.run('MTE2MzkwODk0MzIxNTYwNzgwOA.GWH3bK.9y-b9XVIRtdX7DYiMwuzIr-9UnM-ag2D-wgFcE')
+
+
+
+#Pause command: !pause
+voice_channel = ctx.author.voice.channel
+
+async def pause(ctx):
+    voice_channel = ctx.author.voice.channel
+
+    if voice_channel:
+        voice_client = await voice_channel.connect()
+
+        if voice_client.is_playing():
+            voice_client.pause()
+            await ctx.send('Current song playing paused.')
+        else:
+            await ctx.send('No song playing.')
+        
+        await voice_client.disconnect()
+
+
+# Resume song command: !resume
+async def resume(ctx):
+    
+    resume_text = "Resumes playing the current song."
+    await ctx.send(resume_text)
+
+
+#Skip command: !skip
+async def skip(ctx):
+    voice_channel = ctx.author.voice.channel
+
+    if voice_channel:
+        voice_client = await voice_channel.connect() #Connects to channel
+
+        if voice_client.is_playing():
+            voice_client.stop()
+            await ctx.send('Skipped the current song playing.')
+        else:
+            await ctx.send('No song is playing.')
+
